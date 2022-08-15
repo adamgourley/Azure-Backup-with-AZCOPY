c3f975cdef5c000ae0b56c8c0dc1d9d2a5a8c318
adamgourley <104019246+adamgourley@users.noreply.github.com>
2022-08-15 11:46:49 -0400

    Update backup.ps1

--- backup.ps1
+++ backup.ps1
@@ -12,13 +12,12 @@ while($val -ne 365)
     # Get variables
     $dayinmonth = get-date -format "dd";
     $yearandmonth = get-date -format "yyyy-MM";
-    $dayofweek = (get-date).DayOfWeek
 
     # Create log file
     New-Item -Type File -Path "C:\BackupToAzure\logs" -Name "azcopy-$yearandmonth-$dayinmonth.log"
 
     # Copy the backup to Azure's bsdbackupalpharetta container 'vmbackup'
-    C:\BackupToAzure\azcopy.exe copy "<redacted backup file location>\$dayofweek\*" "<redacted base Azure storage url>/$yearandmonth/$dayinmonth/<redacted credentials>" | Out-File "C:\BackupToAzure\logs\azcopy-$yearandmonth-$dayinmonth.log";
+    C:\BackupToAzure\azcopy.exe copy "<redacted backup file location>\*" "<redacted base Azure storage url>/$yearandmonth/$dayinmonth/<redacted credentials>" | Out-File "C:\BackupToAzure\logs\azcopy-$yearandmonth-$dayinmonth.log";
 
     # Sleep for 24 hours
     Start-Sleep -Seconds 86400
